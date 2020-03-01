b80f0e4f3da794409a6d93a822b433dc44062822
Joshua Pagan <joshuajpagan@gmail.com>
2020-03-01 16:07:40 -0500

    finished node.js

---
+++ express_crash_course/Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name : 'Sam Geoffrey',
+        email : 'sgeoffrey006@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name : 'Carroll Herring',
+        email : 'carroll.herring@gmail.com',
+        status: 'inactive'
+    },
+    {
+        id: 3,
+        name : 'Joshua Pagan',
+        email : 'joshuajpagan@gmail.com',
+        status: 'active'
+    }
+];
+
+module.exports = members;
---
+++ express_crash_course/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const members = require('./Members')
+const logger = require('./middlewar/logger');
+
+const app = express();
+
+// Init middleware
+// app.use(logger);
+
+// Gets All Members
+app.get('/api/members', (req, res) => res.json(members));
+
+// Get Single Member
+app.get('/api/members/:id', (req, res) => {
+    res.send(req.params.id);
+});
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
