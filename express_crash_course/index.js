11048dee96927b46b2ca6b3d291793c52c8ff279
Joshua Pagan <joshuajpagan@gmail.com>
2020-03-01 20:24:29 -0500

    Templete Engine Remains

--- express_crash_course/index.js
+++ express_crash_course/index.js
@@ -1,23 +1,22 @@
 const express = require('express');
 const path = require('path');
-const members = require('./Members')
-const logger = require('./middlewar/logger');
+const logger = require('./middleware/logger');
+
 
 const app = express();
 
 // Init middleware
 // app.use(logger);
 
-// Gets All Members
-app.get('/api/members', (req, res) => res.json(members));
-
-// Get Single Member
-app.get('/api/members/:id', (req, res) => {
-    res.send(req.params.id);
-});
+// Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Members API routes
+app.use('/api/members', require('./routes/api/members'));
 
 const PORT = process.env.PORT || 5000;
 
